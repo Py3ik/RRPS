1b85a233c1bd95a8eda0b14fd4656d12f4021284
Py3ik <kashchko@gmail.com>
2025-07-10 21:10:18 +0200

    Fix

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -7,7 +7,7 @@ from .models import UserProfile
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
-    verbose_name_plural = "Профиль"
+    verbose_name_plural = "Profile"
 
 
 class CustomUserAdmin(UserAdmin):
